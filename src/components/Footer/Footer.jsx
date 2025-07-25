1ab9f15cf48127ee404331eb8efe1b706f086ee5
unknown <Abhinavkulkarni997>
2025-07-25 18:01:08 +0530

    new files and features added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -409,36 +409,39 @@ const renderColumn = (columnData) =>
       
       {/* Footer content */}
       {/* <div className="grid grid-cols-1 justify-center max-w-[1240px] gap-6  mx-auto py-16 px-4 gap-x-1 md:grid-cols-4 sm:grid-cols-5 lg:grid-cols-5"> */}
-            <div className="grid grid-cols-1  sm:grid-cols-2  max-w-[1240px] mx-auto gap-6  py-16 px-4 md:grid-cols-3  lg:grid-cols-6  ">
+            <div className="max-w-[1240px]  mx-auto py-16 px-4 grid grid-cols-1  sm:grid-cols-2   gap-6   md:grid-cols-3  lg:grid-cols-6  ">
         <div  className="flex justify-center sm:justify-start">
           <img
             src={logo1}
             alt="logo"
             // style={{ height: 150, width: 150 }}
-            className="h-24 w-24 md:h-24 md:w-24"
+            className="h-36 w-36 md:h-36 md:w-36"
           />
         </div>
-        <div className="sm:col-span-1 md:col-span-2 lg:col-span-4  grid grid-cols-1 sm:grid-cols-2 md: ">
+        <div className="sm:col-span-1 md:col-span-2 lg:col-span-4  grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 text-sm">
         
-          <ul className="space-y-2 mt-2 min-w-[200px]" role="list"></ul>
-    
+          <ul className="space-y-2 " role="list">{renderColumn(column1)}</ul>
+            <ul className="space-y-2 " role="list">{renderColumn(column2)}</ul>
+              <ul className="space-y-2 " role="list">{renderColumn(column3)}</ul>
+                <ul className="space-y-2 " role="list">{renderColumn(column4)}</ul>
+                  <ul className="space-y-2 " role="list">{renderColumn(column5)}</ul>
 
         </div>
           
 
           {/* Social Media section */}
           {/* <div className="flex flex-col space-y-4 "> */}
-           <div className="col-span-full lg:col-span-1 flex flex-col items-end lg:items-start mt-8 lg:mt-0 space-y-4 ">
-            <p className="text-2xl md:text-[30px] font-bold text-white text-right whitespace-nowrap">
+           <div className="  flex flex-col items-center  lg:items-end mt-8 lg:mt-0 space-y-4 ">
+            <p className="text-xl md:text-2xl font-bold  text-center lg:text-right">
               Find Us Online
             </p>
-            <div className="flex flex-row space-x-4 ">
+            <div className="flex space-x-4 ">
               {/* Facebook */}
               <a
                 href="https://facebook.com/csirngrihyd"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="relative group w-12 h-12 flex items-center justify-center rounded-full overflow-hidden bg-transparent transition duration-500 shadow-md "
+                className=" group w-10 h-10 flex items-center justify-center rounded-full overflow-hidden bg-transparent transition duration-500 shadow-md "
               >
                 <span className="absolute inset-0 bg-gradient-to-br from-blue-500 to-blue-700 transform scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0 rounded-full "></span>
                 <FaSquareFacebook
@@ -452,7 +455,7 @@ const renderColumn = (columnData) =>
                 href="https://x.com/csirngri"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="relative group w-12 h-12 flex items-center justify-center rounded-full overflow-hidden bg-transparent transition duration-500 shadow-md "
+                className="group w-10 h-10 flex items-center justify-center rounded-full overflow-hidden bg-transparent transition duration-500 shadow-md "
               >
                 <span className="absolute inset-0 bg-gradient-to-br from-gray-900 to-black transform scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0 rounded-full "></span>
                 <FaSquareXTwitter
@@ -466,7 +469,7 @@ const renderColumn = (columnData) =>
                 href="https://youtube.com/@csir-ngri"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="relative group w-12 h-12 flex items-center justify-center rounded-full overflow-hidden bg-transparent transition duration-500 shadow-md"
+                className=" group w-10 h-10 flex items-center justify-center rounded-full overflow-hidden bg-transparent transition duration-500 shadow-md"
               >
                 <span className="absolute inset-0 bg-gradient-to-br from-red-500 to-red-700 transform scale-0 group-hover:scale-100 transition-transform duration-500 ease-in-out z-0 rounded-full"></span>
                 <FaYoutubeSquare
@@ -476,21 +479,19 @@ const renderColumn = (columnData) =>
               </a>
             </div>
             {/* Visitors Count Section */}
-            <div className="flex items-center gap-x-2 mt-6 white-space-nowrap">
-              <span className="text-white">
+            <div className=" mt-4 ">
                 <VisitorsCount />
-              </span>
             </div>
           </div>
        
       </div>
 
       {/* Bottom section: All rights reserved*/}
-      <div className="border-t   text-white py-8 px-4 border-white/20 ">
-        <p className="text-xs sm:text-sm font-semibold max-w-4xl mx-auto loading-relaxed">
+      <div className="border-t border-white/20 text-center  text-white py-6 px-4 text-sm  ">
+   
           © 2025 Crafted and maintained by CSIR-NGRI's IT Division, Hyderabad.
           All rights reserved.
-        </p>
+    
       </div>
     </footer>
   );
