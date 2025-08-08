785e35ae0a7dce9fe9c2d926722b9204c9f7223d
unknown <Abhinavkulkarni997>
2025-08-08 18:03:59 +0530

    new files and features added

--- src/components/ResearchNewsCard/ResearchNewsCard.jsx
+++ src/components/ResearchNewsCard/ResearchNewsCard.jsx
@@ -11,8 +11,8 @@ const ResearchNewsCard = () => {
       whileHover={{ scale: 1.02 }}
     className="bg-white max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl min-h-fit h-auto border border-gray-200 rounded-md shadow-lg py-8 sm:py-12 md:py-16 px-4 sm:px-6 md:px-8  mt-8 sm:mt-12 md:mt-16 mx-auto sm:ml-4 md:ml-6 lg:ml-8">
       <Link to="https://www.ngri.res.in/cms/rnews.php" target="_blank" rel="noopener noreferrer">
-          <img src={renews} alt="ResearchNews" className="w-28 h-28 mx-auto object-contain rounded-md" />
-          <a href="#" className="block text-center text-lg sm:text-xl md:text-2xl  text-blue-500 hover:text-blue-700 font-semibold mt-4">Research News</a>
+          <img src={renews} alt="ResearchNews" className="w-20 h-20 sm:h-24 sm:w-24 md:w-28 md:h-28 lg:h-32 mx-auto object-contain rounded-md" />
+          <a href="#" className="block text-center text-lg sm:text-xl md:text-2xl  text-blue-500 hover:text-blue-700 font-semibold mt-3 sm:mt-4 md:mt-6 transition-colors">Research News</a>
           </Link>
           <p className="text-sm leading-relaxed text-center text-gray-700 font-serif mt-4 ">Explore Monthly Research Papers for the Latest Insights</p>
       </motion.div>
