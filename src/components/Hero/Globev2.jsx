de704f6c1d4a4ab52e41a6664d991e6aa888d0ad
unknown <Abhinavkulkarni997>
2025-09-01 18:00:16 +0530

    new files and features added

--- src/components/Hero/Globev2.jsx
+++ src/components/Hero/Globev2.jsx
@@ -731,8 +731,8 @@ export default function GlobeRealistic() {
       {/* <div className="w-full h-full relative" style={{ backgroundImage: `url(${HeroBanner})`,backgroundSize:'cover',backgroundPosition:'center',backgroundRepeat:'no-repeat' }}></div> */}
      <div className="flex items-center justify-center absolute inset-0 z-10 ">
      {/* <img src={HeroBanner} alt="Hero_Banner" className="max-w-4xl  w-full h-full  opacity-100 rounded-full  p-8 " /> */}
-     {/* <img src={HeroBanner} alt="Hero_Banner" className="max-w-4xl  w-full h-full  opacity-50 backdrop-blur-md bg-white/20 shadow-2xl  " /> */}
-     <img src={Banner} alt="Hero_Banner" className="max-w-4xl  w-full h-full  opacity-50 backdrop-blur-md bg-white/20 shadow-2xl  rounded-full" />
+     {/* <img src={HeroBanner} alt="Hero_Banner" className="max-w-8xl  w-full h-full  opacity-50 backdrop-blur-md bg-white/20 shadow-2xl  " /> */}
+     <img src={Banner} alt="Hero_Banner" className="max-w-8xl  w-full h-full  opacity-50 backdrop-blur-md bg-white/20 shadow-2xl  " />
      </div>
       <Canvas 
       className="absolute inset-0 z-0"
